a6ebe82cb8130436d0eadd041a7eb925b5b62236
Oceanshiver <62504973+Oceanshiver@users.noreply.github.com>
2023-04-10 14:10:53 +0300

    add paged ant grid

---
+++ src/AntExperiments/Pages/AntGridPage.razor.cs
@@ -0,0 +1,63 @@
+﻿using AntDesign;
+using Bogus;
+
+namespace AntExperiments.Pages;
+
+public partial class AntGridPage
+{
+    public bool IsLoading { get; set; }
+    public int Total { get; set; }
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+    public List<Model>? Items { get; set; }
+
+    private List<Model>? _testData;
+
+    protected override void OnInitialized()
+    {
+        _testData = GenerateTestData(15);
+
+        Total = _testData.Count;
+        PageSize = 5;
+
+        LoadPagedData(1, PageSize);
+    }
+
+    private void LoadPagedData(int page, int pageSize)
+    {
+        Items = _testData?.Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+        PageIndex = page;
+    }
+
+    private void OnPageChanged(PaginationEventArgs p)
+        => LoadPagedData(p.Page, p.PageSize);
+
+    private List<Model> GenerateTestData(int size)
+    {
+        var testData = new List<Model>();
+        var model = new Faker<Model>()
+            .RuleFor(m => m.Id, f => f.IndexFaker)
+            .RuleFor(m => m.FirstName, (f, _) => f.Name.FirstName())
+            .RuleFor(m => m.LastName, (f, _) => f.Name.LastName())
+            .RuleFor(m => m.Email, (f, m) => f.Internet.Email(m.FirstName, m.LastName))
+            .RuleFor(m => m.Value, f => $"Value {f.UniqueIndex}");
+
+        for (var i = 0; i < size; i++)
+        {
+            testData.Add(model.Generate());
+        }
+
+        return testData.OrderBy(x => x.Id).ToList();
+    }
+}
+
+public class Model
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? Value { get; set; }
+}
